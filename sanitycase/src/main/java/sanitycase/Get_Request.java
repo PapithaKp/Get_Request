fda48822e118442d4f5fc25ee1671075ac95d2f6
PapithaK <PapithaK@DESK-1986.sulekha.net>
2019-10-01 11:52:51 +0530

    Initial Commit

---
+++ sanitycase/src/main/java/sanitycase/Get_Request.java
@@ -0,0 +1,19 @@
+package sanitycase;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Get_Request {
+
+	public static void main(String[] args) {
+		// Get_Request
+
+		RestAssured.baseURI = "http://lsapilistings.sulekha.com";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("/apiv2/Businesses/10253358/Campaigns/1555793/Threshold");
+		int resp = response.getStatusCode();
+		System.out.println("Status code is  " + resp + "  and Status message is " + response.asString());
+
+	}
+}
